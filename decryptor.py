58e1e0aa0d7773633b0b2f4c20186d94ff68777d
Silky <40408435+S1lkys@users.noreply.github.com>
2019-07-05 00:22:34 +0200

    Create decryptor.py

---
+++ decryptor.py
@@ -0,0 +1,61 @@
+
+import base64
+import hashlib
+import os
+
+import six
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import Cipher
+from cryptography.hazmat.primitives.ciphers.algorithms import AES
+from cryptography.hazmat.primitives.ciphers.modes import CFB8
+
+padding_string = b']'
+iv_size = AES.block_size //8
+
+
+def decrypt(ciphertext, key):
+    """
+    Decrypt the AES encrypted string.
+    Parameters:
+        ciphertext -- Encrypted string with AES method.
+        key        -- key to decrypt the encrypted string.
+    """
+
+    ciphertext = base64.b64decode(ciphertext)
+    iv = ciphertext[:iv_size]
+
+    cipher = Cipher(AES(pad(key)), CFB8(iv), default_backend())
+    decryptor = cipher.decryptor()
+    return decryptor.update(ciphertext[iv_size:]) + decryptor.finalize()
+
+
+
+
+
+def pad(key):
+    """Add padding to the key."""
+
+    if isinstance(key, six.text_type):
+        key = key.encode()
+
+    # Key must be maximum 32 bytes long, so take first 32 bytes
+    key = key[:32]
+
+    # If key size is 16, 24 or 32 bytes then padding is not required
+    if len(key) in (16, 24, 32):
+        return key
+
+    # Add padding to make key 32 bytes long
+    return key.ljust(32, padding_string)
+
+
+
+user_hash = "$pbkdf2-sha512$25000$z9nbm1Oq9Z5TytkbQ8h5Dw$Vtx9YWQsgwdXpBnsa8BtO5kLOdQGflIZOQysAy7JdTVcRbv/6csQHAJCAIJT9rLFBawClFyMKnqKNL5t3Le9vg" # change this
+decrypt_key =  "utUU0jkamCZDmqFLOrAuPjFxL0zp8zWzISe5MF0GY/l8Silrmu3caqrtjaVjLQlvFFEgESGz"             # change this
+
+
+
+passwd = decrypt(decrypt_key,user_hash)
+
+print (passwd)
